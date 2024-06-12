6fffbdb0d347a60a58db57f9556e10591e3fe09b
Pascal Brilliandy Kristianto <pascalbrilliandy@gmail.com>
2024-06-13 00:27:43 +0700

    Update

--- app.py
+++ app.py
@@ -1,125 +1,214 @@
-from flask import Flask, request, jsonify
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 import pandas as pd
-import numpy as np
 import re
-import string
 import nltk
-from nltk.corpus import stopwords
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import load_model
-import tensorflow as tf
+import string
 import pickle
+import numpy as np
+import sqlite3
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from num2words import num2words
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.preprocessing.text import Tokenizer
+from collections import defaultdict
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.utils import to_categorical
+from flask import Flask, jsonify
+from flask import request
+from flasgger import Swagger, LazyString, LazyJSONEncoder
+from flasgger import swag_from
 
 app = Flask(__name__)
 
-# Load the models
-model_nn = load_model('Neural Network.h5')
-model_lstm = load_model('model_lstm.h5')
-
-# Load the tokenizer
-with open('/Users/admin/Documents/Platinum Challenge/tokenizer.pickle', 'rb') as f:
-    tokenizer = pickle.load(f)
-
-# Preprocess function
-def preprocess_text(text):
-    stop_words = set(stopwords.words('indonesian'))
-    text = text.lower()                                 # Mengubah menjadi huruf kecil
-    text = re.sub(r'\s+', ' ', text)                    # Mengganti banyak spasi dengan satu spasi
-    text = re.sub(r'[^a-zA-Z\s]', '', text)             # Menghapus karakter spesial kecuali spasi
-    text = re.sub(r'\t', ' ', text)                     # Menghapus tab dan menggantinya dengan spasi
-    text = re.sub(r'\d+', '', text)                     # Menghapus angka
-    text = re.sub(f"[{string.punctuation}]", "", text)  # Menghapus tanda baca
-    text = ' '.join([word for word in text.split() if word not in stop_words])  # Menghapus stopwords
+def clean_text(text):
+    text = re.sub(r'\\t|\\n|\\u', ' ', text) #Menghapus karakter khusus seperti tab, baris baru, karakter Unicode, dan backslash.
+    text = re.sub(r"https?:[^\s]+", ' ', text)  # Menghapus http / https
+    text = re.sub(r'(\b\w+)-\1\b', r'\1', text)
+    text = re.sub(r'[\\x]+[a-z0-9]{2}', '', text)  # Menghapus karakter yang dimulai dengan '\x' diikuti oleh dua karakter huruf atau angka
+    # text = re.sub(r'(\d+)', r' \1 ', text)  # Memisahkan angka dari teks
+    text = re.sub(r'[^a-zA-Z]+', ' ', text)  # Menghapus karakter kecuali huruf, dan spasi
+    text = re.sub(r'\brt\b|\buser\b', ' ', text) # Menghapus kata-kata 'rt' dan 'user'
+    text = text.lower()
     return text
 
-@app.route('/train', methods=['POST'])
-def train_model():
-    # Load Dataset
-    data = pd.read_csv('/train_preprocess.tsv.txt', sep='\t', header=None, names=['text', 'label'])
+alay_df = pd.read_csv('new_kamusalay.csv', encoding='latin-1', header=None)
+alay_filter = dict(zip(alay_df[0], alay_df[1]))
 
-    # Preprocess Data
-    data['cleaned_text'] = data['text'].apply(preprocess_text)
-    sequences = tokenizer.texts_to_sequences(data['cleaned_text'])
-    maxlen = max([len(seq) for seq in sequences])
-    X = pad_sequences(sequences, maxlen=maxlen)
-    y = data['label'].values
+def normalisasi_alay(text):
+    return ' '.join(alay_filter.get(word, word) for word in text.split(' '))
 
-    # Determine input_dim and set output_dim, vocab_size
-    input_dim = len(tokenizer.word_index) + 1
-    output_dim = 128
-    vocab_size = len(tokenizer.word_index) + 1
+factory = StemmerFactory()
+stemer = factory.create_stemmer()
 
-    # Split data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=123)
+list_stopwords = set(stopwords.words('indonesian'))
 
-    # Fit and transform labels to one-hot encoded format
-    onehot_encoder = OneHotEncoder()
-    y_train = y_train.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
-    y_train_hot = onehot_encoder.fit_transform(y_train).toarray()
-    y_test_hot = onehot_encoder.transform(y_test).toarray()
+# Tokenizing
+def tokenize(text):
+    return word_tokenize(text)
 
-    # Train the model
-    es = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', patience=80, restore_best_weights=True)
-    history = model_lstm.fit(X_train, y_train_hot, epochs=100, validation_data=(X_test, y_test_hot), batch_size=64, callbacks=[es])
-    return jsonify({'message': 'Model trained successfully!'})
+# Removing stopwords
+def remove_stopwords(text):
+    return [word for word in text if not word in list_stopwords]
 
-@app.route('/evaluate', methods=['POST'])
-def evaluate_model():
-    # Get the test data
-    test_data = request.json['test_data']
-    sequences = tokenizer.texts_to_sequences(test_data)
-    maxlen = max([len(seq) for seq in sequences])
-    X = pad_sequences(sequences, maxlen=maxlen)
+# Stemming
+def stemming(text):
+    return [stemer.stem(word) for word in text]
 
-    # Get the true labels
-    y_true_lstm = np.array(request.json['y_true_lstm'])
+# Convert list of words to a sentence
+def words_to_sentence(list_words):
+    return ' '.join(list_words)
 
-    # Evaluate the model
-    loss, accuracy = model_lstm.evaluate(X, y_true_lstm)
+def text_preprocessing(text):
+    text = tokenize(text)
+    text = remove_stopwords(text)
+    text = stemming(text)
+    text = words_to_sentence(text)
+    return text
 
-    return jsonify({'loss': loss, 'accuracy': accuracy})
+app.json_encoder = LazyJSONEncoder
+swagger_template = dict(
+info = {
+    'title': 'Pascal Gold Challenge',
+    'version': '1.0.0',
+    'description': 'Dokumentasi API untuk Data Processing dan Modeling',
+    },
+#     host = "127.0.0.1:5000"
+#     host = LazyString(lambda: request.host)
+)
 
-@app.route('/predict_lstm', methods=['POST'])
-def predict():
-    # Get the input text
-    input_text = request.json['text']
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'docs',
+            "route": '/docs.json',
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs/"
+}
 
-    # Cleaned text
-    cleaned_text = preprocess_text(input_text)
+swagger = Swagger(app, template=swagger_template,             
+                  config=swagger_config)
 
-    # Tokenize the text
-    input_sequence = tokenizer.texts_to_sequences([cleaned_text])
-    maxlen = max([len(seq) for seq in input_sequence])
-    X = pad_sequences(input_sequence, maxlen=maxlen)
+@swag_from("docs/hello_world.yml", methods=['GET'])
+@app.route('/', methods=['GET'])
+def hello_world():
+    json_response = {
+        'status_code': 200,
+        'description': "Menyapa Hello World",
+        'data': "Hello World",
+    }
 
-    # Make prediction
-    y_pred_lstm = model_lstm.predict(X)
-    y_pred_lstm = np.argmax(y_pred_lstm)
+    response_data = jsonify(json_response)
+    return response_data
 
-    return jsonify({'y_pred_lstm': int(y_pred_lstm)})
+# @swag_from("C://Users/Admin/Documents/GitHub/231000001-19-pbk-cleansing-gold/docs/text.yml", methods=['GET'])
+# @app.route('/text', methods=['GET'])
+# def text():
+#     json_response = {
+#         'status_code': 200,
+#         'description': "Original Teks",
+#         'data': "Halo, apa kabar semua?",
+#     }
 
-@app.route('/predict_nn', methods=['POST'])
-def predict_nn():
-    # Get the input text
-    input_text = request.json['text']
+#     response_data = jsonify(json_response)
+#     return response_data
 
-    # Preprocess the text
-    cleaned_text = preprocess_text(input_text)
+# @swag_from("C://Users/Admin/Documents/GitHub/231000001-19-pbk-cleansing-gold/docs/text_clean.yml", methods=['GET'])
+# @app.route('/text-clean', methods=['GET'])
+# def text_clean():
+#     json_response = {
+#         'status_code': 200,
+#         'description': "Teks yang sudah dibersihkan",
+#         'data': re.sub(r'[^a-zA-Z0-9]', ' ', "Halo, apa kabar semua?"),
+#     }
 
-    # Tokenize the text
-    input_sequence = tokenizer.texts_to_sequences([cleaned_text])
-    maxlen = max([len(seq) for seq in input_sequence])
-    X = pad_sequences(input_sequence, maxlen=maxlen)
+#     response_data = jsonify(json_response)
+#     return response_data
 
-    # Make prediction using Neural Network model
-    y_pred_nn = model_nn.predict(X)
-    y_pred_nn = np.argmax(y_pred_nn)
+@swag_from("docs/text_processing.yml", methods=['POST'])
+@app.route('/text-processing', methods=['POST'])
+def text_processing():
+    text = request.form.get('text')
+    cleaned_text = clean_text(text)
+    normalized_text = normalisasi_alay(cleaned_text)
 
-    return jsonify({'y_pred_nn': int(y_pred_nn)})
+    conn = sqlite3.connect('binar_gold.db')
+    cursor = conn.cursor()
+
+    # Membuat tabel cleaned_text jika belum ada
+    cursor.execute('create table if not exists insert_text(id integer primary key autoincrement, text text)')
+    
+    # Memasukkan data ke dalam tabel
+    cursor.execute('insert into insert_text(text) values (?)', (normalized_text,))
+    
+    conn.commit()
+    conn.close()
+
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': normalized_text,
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
+
+@swag_from("docs/text_processing_file.yml", methods=['POST'])
+@app.route('/text-processing-file', methods=['POST'])
+def text_processing_file():
+    # Upladed file
+    file = request.files.getlist('file')[0]
+
+    # Import file csv ke Pandas
+    data = pd.read_csv('data.csv', encoding='latin-1')
+
+    # Ambil teks yang akan diproses dalam format list
+    texts = data.Tweet.to_list()
+
+    # Lakukan cleansing pada teks
+    cleaned_text = []
+    for text in texts:
+        text = clean_text(text)
+        text = normalisasi_alay(text)
+        cleaned_text.append(text)
+
+    conn = sqlite3.connect('binar_gold.db')
+    cursor = conn.cursor()
+
+    # Membuat tabel cleaned_text
+    cursor.execute('create table if not exists cleaned_text(id integer primary key autoincrement, text text)')
+
+    # Insert data di tabel cleaned_text
+    for text in cleaned_text:
+        cursor.execute('insert into cleaned_text(text) values (?)', (text,))
+
+    conn.commit()
+    conn.close()
+
+    json_response = {
+        'status_code': 200,
+        'description': "Teks yang sudah diproses",
+        'data': cleaned_text,
+    }
+
+    response_data = jsonify(json_response)
+    return response_data
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run()
