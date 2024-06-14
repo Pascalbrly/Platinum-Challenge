cc04b9105a371f96375b895d733b0006b02101be
Pascal Brilliandy Kristianto <pascalbrilliandy@gmail.com>
2024-06-14 17:27:08 +0700

    Update

--- app.py
+++ app.py
@@ -28,60 +28,47 @@ from flask import Flask, jsonify
 from flask import request
 from flasgger import Swagger, LazyString, LazyJSONEncoder
 from flasgger import swag_from
+from keras.models import load_model
 
-app = Flask(__name__)
-
-def clean_text(text):
-    text = re.sub(r'\\t|\\n|\\u', ' ', text) #Menghapus karakter khusus seperti tab, baris baru, karakter Unicode, dan backslash.
-    text = re.sub(r"https?:[^\s]+", ' ', text)  # Menghapus http / https
-    text = re.sub(r'(\b\w+)-\1\b', r'\1', text)
-    text = re.sub(r'[\\x]+[a-z0-9]{2}', '', text)  # Menghapus karakter yang dimulai dengan '\x' diikuti oleh dua karakter huruf atau angka
-    # text = re.sub(r'(\d+)', r' \1 ', text)  # Memisahkan angka dari teks
-    text = re.sub(r'[^a-zA-Z]+', ' ', text)  # Menghapus karakter kecuali huruf, dan spasi
-    text = re.sub(r'\brt\b|\buser\b', ' ', text) # Menghapus kata-kata 'rt' dan 'user'
-    text = text.lower()
-    return text
-
-alay_df = pd.read_csv('new_kamusalay.csv', encoding='latin-1', header=None)
-alay_filter = dict(zip(alay_df[0], alay_df[1]))
-
-def normalisasi_alay(text):
-    return ' '.join(alay_filter.get(word, word) for word in text.split(' '))
 
 factory = StemmerFactory()
-stemer = factory.create_stemmer()
-
+stemmer = factory.create_stemmer()
 list_stopwords = set(stopwords.words('indonesian'))
 
-# Tokenizing
-def tokenize(text):
-    return word_tokenize(text)
+# Load your model and tokenizer
+model_nn = load_model('nn_model.h5')
+# Memuat model LSTM
+model_lstm = load_model('lstm_model.h5')
 
-# Removing stopwords
-def remove_stopwords(text):
-    return [word for word in text if not word in list_stopwords]
+# Memuat tokenizer (jika belum dimuat di bagian lain)
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
 
-# Stemming
-def stemming(text):
-    return [stemer.stem(word) for word in text]
+class_labels = ['negatif', 'netral', 'positif']
 
-# Convert list of words to a sentence
-def words_to_sentence(list_words):
-    return ' '.join(list_words)
-
-def text_preprocessing(text):
-    text = tokenize(text)
-    text = remove_stopwords(text)
-    text = stemming(text)
-    text = words_to_sentence(text)
+# Function for text preprocessing
+def cleansing(text):
+    text = re.sub(r'\\t|\\n|\\u', ' ', text)
+    text = re.sub(r"https?:[^\s]+", ' ', text)
+    text = re.sub(r'(\b\w+)-\1\b', r'\1', text)
+    text = re.sub(r'[\\x]+[a-z0-9]{2}', '', text)
+    text = re.sub(r'[^a-zA-Z]+', ' ', text)
+    text = re.sub(r'\brt\b|\buser\b', ' ', text)
+    text = text.lower()
+    tokens = nltk.word_tokenize(text)
+    tokens = [word for word in tokens if word not in list_stopwords]
+    tokens = [stemmer.stem(word) for word in tokens]
+    text = ' '.join(tokens)
     return text
+
+app = Flask(__name__)
 
 app.json_encoder = LazyJSONEncoder
 swagger_template = dict(
 info = {
-    'title': 'Pascal Gold Challenge',
-    'version': '1.0.0',
-    'description': 'Dokumentasi API untuk Data Processing dan Modeling',
+    'title': 'API documentation for ML and DL',
+    'version': '1.0.1',
+    'description': 'API for sentiment prediction using keras NN, LSTM, and MLPClassifier models',
     },
 #     host = "127.0.0.1:5000"
 #     host = LazyString(lambda: request.host)
@@ -107,108 +94,54 @@ swagger = Swagger(app, template=swagger_template,
 @app.route('/', methods=['GET'])
 def hello_world():
     json_response = {
-        'status_code': 200,
-        'description': "Menyapa Hello World",
-        'data': "Hello World",
+        'Platinum Challenge': "Tweet Sentiment Analysis using keras NN, LSTM, and MLPClassifier models",
     }
 
     response_data = jsonify(json_response)
     return response_data
 
-# @swag_from("C://Users/Admin/Documents/GitHub/231000001-19-pbk-cleansing-gold/docs/text.yml", methods=['GET'])
-# @app.route('/text', methods=['GET'])
-# def text():
-#     json_response = {
-#         'status_code': 200,
-#         'description': "Original Teks",
-#         'data': "Halo, apa kabar semua?",
-#     }
-
-#     response_data = jsonify(json_response)
-#     return response_data
-
-# @swag_from("C://Users/Admin/Documents/GitHub/231000001-19-pbk-cleansing-gold/docs/text_clean.yml", methods=['GET'])
-# @app.route('/text-clean', methods=['GET'])
-# def text_clean():
-#     json_response = {
-#         'status_code': 200,
-#         'description': "Teks yang sudah dibersihkan",
-#         'data': re.sub(r'[^a-zA-Z0-9]', ' ', "Halo, apa kabar semua?"),
-#     }
-
-#     response_data = jsonify(json_response)
-#     return response_data
-
-@swag_from("docs/text_processing.yml", methods=['POST'])
-@app.route('/text-processing', methods=['POST'])
-def text_processing():
-    text = request.form.get('text')
-    cleaned_text = clean_text(text)
-    normalized_text = normalisasi_alay(cleaned_text)
-
-    conn = sqlite3.connect('binar_gold.db')
-    cursor = conn.cursor()
-
-    # Membuat tabel cleaned_text jika belum ada
-    cursor.execute('create table if not exists insert_text(id integer primary key autoincrement, text text)')
-    
-    # Memasukkan data ke dalam tabel
-    cursor.execute('insert into insert_text(text) values (?)', (normalized_text,))
-    
-    conn.commit()
-    conn.close()
+@swag_from('docs/nn.yaml', methods=['POST'])
+@app.route('/nn', methods=['POST'])
+def nn():
+    original_text = request.form.get('text')
+    cleaned_text = cleansing(original_text)
+    text_sequence = tokenizer.texts_to_sequences([cleaned_text])
+    text_padded = pad_sequences(text_sequence, maxlen=64)  # Sesuaikan maxlen sesuai kebutuhan Anda
+    prediction = model_nn.predict(text_padded)
+    predicted_class = np.argmax(prediction[0])
+    get_sentiment = class_labels[predicted_class]
 
     json_response = {
         'status_code': 200,
-        'description': "Teks yang sudah diproses",
-        'data': normalized_text,
+        'description': "NN Prediction Result",
+        'data': {
+            'text': original_text,
+            'sentiment': get_sentiment,
+        }
     }
+    return jsonify(json_response)
 
-    response_data = jsonify(json_response)
-    return response_data
-
-
-@swag_from("docs/text_processing_file.yml", methods=['POST'])
-@app.route('/text-processing-file', methods=['POST'])
-def text_processing_file():
-    # Upladed file
-    file = request.files.getlist('file')[0]
-
-    # Import file csv ke Pandas
-    data = pd.read_csv('data.csv', encoding='latin-1')
-
-    # Ambil teks yang akan diproses dalam format list
-    texts = data.Tweet.to_list()
-
-    # Lakukan cleansing pada teks
-    cleaned_text = []
-    for text in texts:
-        text = clean_text(text)
-        text = normalisasi_alay(text)
-        cleaned_text.append(text)
-
-    conn = sqlite3.connect('binar_gold.db')
-    cursor = conn.cursor()
-
-    # Membuat tabel cleaned_text
-    cursor.execute('create table if not exists cleaned_text(id integer primary key autoincrement, text text)')
-
-    # Insert data di tabel cleaned_text
-    for text in cleaned_text:
-        cursor.execute('insert into cleaned_text(text) values (?)', (text,))
-
-    conn.commit()
-    conn.close()
+# Define the /lstm route
+@swag_from('docs/lstm.yaml', methods=['POST'])
+@app.route('/lstm', methods=['POST'])
+def lstm():
+    original_text = request.form.get('text')
+    cleaned_text = cleansing(original_text)
+    text_sequence = tokenizer.texts_to_sequences([cleaned_text])
+    text_padded = pad_sequences(text_sequence, maxlen=64)  # Sesuaikan maxlen sesuai kebutuhan Anda
+    prediction = model_lstm.predict(text_padded)
+    predicted_class = np.argmax(prediction[0])
+    get_sentiment = class_labels[predicted_class]
 
     json_response = {
         'status_code': 200,
-        'description': "Teks yang sudah diproses",
-        'data': cleaned_text,
+        'description': "LSTM Prediction Result",
+        'data': {
+            'text': original_text,
+            'sentiment': get_sentiment,
+        }
     }
-
-    response_data = jsonify(json_response)
-    return response_data
-
+    return jsonify(json_response)
 
 if __name__ == '__main__':
    app.run()
